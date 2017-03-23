1bdb392aa20a6753c4efd1523a94ff6df2bc7eb2
Mone Rivas <mone.rivas@gmail.com>
2017-03-23 17:01:46 -0600

    actualizacion: ya hace las 4 operaciones basicas, pero para que las haga por separado hay que refrescar la pagina entre cada operacion

--- CALCULADORA1.js
+++ CALCULADORA1.js
@@ -28,6 +28,25 @@ function suma(){
     document.getElementById('salida').value = '';
 }
 
+function resta(){
+    //var operador = document.getElementById('suma');
+
+    var valorCajita = document.getElementById('salida').value;
+    var resta = 0;
+    if(arreglo[0] == undefined){
+        arreglo[0] = valorCajita;
+        console.log(arreglo);
+    }else{
+        arreglo[1] = valorCajita;
+        console.log(arreglo);
+        resta = parseInt(arreglo[0]) - parseInt(arreglo[1]);
+        arreglo[0] = resta;
+        console.log(resta);
+        // arreglo=[];
+    }
+    document.getElementById('salida').value = '';
+}
+
 function producto(){
     //var operador = document.getElementById('suma');
 
@@ -43,8 +62,24 @@ function producto(){
         arreglo[0] = producto;
         console.log(producto);
         // arreglo=[];
-
     }
     document.getElementById('salida').value = '';
+}
+function division(){
+    //var operador = document.getElementById('suma');
 
+    var valorCajita = document.getElementById('salida').value;
+    var division = 1;
+    if(arreglo[0] == undefined){
+        arreglo[0] = valorCajita;
+        console.log(arreglo);
+    }else{
+        arreglo[1] = valorCajita;
+        console.log(arreglo);
+        division = parseInt(arreglo[0]) / parseInt(arreglo[1]);
+        arreglo[0] = division;
+        console.log(division);
+        // arreglo=[];
+    }
+    document.getElementById('salida').value = '';
 }
